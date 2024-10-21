b0c829ee564979dcbe4e31545e5600eb18388b5e
Vespid <farid.elmam@gmail.com>
2024-10-21 14:19:41 +0200

    delette messages

---
+++ Hakim/routes/access-token.mjs
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken';
+
+const JWT_KEY = process.env.JWT_KEY;
+
+const verifyToken = (req, res, next) => {
+    const token = req.headers['access-token'];
+
+    if(!token) {
+        return res.send({success: false, message: "Token manquant"});
+    }
+
+    jwt.verify(token, JWT_KEY, (err, decoded) => {
+        if(err) {
+            return res.send({success: false, message: 'Token invalide'});
+        }
+
+        req.user = decoded; //contien l'userID et l'email (et le password?) et devra remplacer les req.params.userId en req.user.userId
+        next();
+    })
+};
+
+export default verifyToken;
